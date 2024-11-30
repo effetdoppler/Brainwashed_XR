95b001ac0d832c2df9403d1269576feb94c7874b
OplizZombie <167470621+OplizZombie@users.noreply.github.com>
2024-11-30 19:16:42 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,33 +4,49 @@ import glsl from 'vite-plugin-glsl';
 import path from 'path';
 
 export default defineConfig({
+    // Chemin de base pour le déploiement sur GitHub Pages
     base: "/Brainwashed_XR/",
+
+    // Suppression des messages inutiles dans la console
     clearScreen: false,
+
+    // Configuration de l'optimisation des dépendances
     optimizeDeps: {
         esbuildOptions: {
             supported: {
-                'top-level-await': true
+                'top-level-await': true // Support de "top-level await"
             }
         }
     },
+
+    // Configuration ESBuild pour le support avancé
     esbuild: {
         supported: {
             'top-level-await': true
         }
     },
+
+    // Configuration de construction
     build: {
-        sourcemap: true,
+        sourcemap: true, // Générer des sourcemaps pour faciliter le débogage
     },
+
+    // Configuration du serveur local
     server: {
-        open: true
+        open: true // Ouvrir automatiquement dans le navigateur
     },
+
+    // Alias pour simplifier les importations
     resolve: {
         alias: {
-            'three': path.resolve('./node_modules/three'),
+            'three': path.resolve('./node_modules/three'), // Alias pour le module "three"
         }
     },
+
+    // Plugins
     plugins: [
         viteStaticCopy({
+            // Copier des fichiers nécessaires dans le dossier de build
             targets: [
                 { src: 'node_modules/three/examples/jsm/libs/ammo.wasm.js', dest: 'jsm/libs/' },
                 { src: 'node_modules/three/examples/jsm/libs/ammo.wasm.wasm', dest: 'jsm/libs/' },
@@ -40,6 +56,6 @@ export default defineConfig({
                 { src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_wasm_wrapper.js', dest: 'jsm/libs/draco/gltf/' }
             ]
         }),
-        glsl()
+        glsl() // Support pour les shaders GLSL
     ]
 });
